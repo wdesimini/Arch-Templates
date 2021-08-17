904f9790acc50bcdb53385ce31e0caed4fce08ce
Wilson Desimini <wdesimini8@gmail.com>
2021-08-17 09:55:11 -0700

    add mvvm swiftui template

---
+++ WillyMVVM_SwiftUI.xctemplate/___VARIABLE_moduleIdentifier___Model.swift
@@ -0,0 +1,10 @@
+//
+//  ___VARIABLE_moduleIdentifier___Model.swift
+//  
+//
+//  Created by Desimini, Wilson on 7/23/21.
+//
+
+struct ___VARIABLE_moduleIdentifier___Model {
+    
+}
---
+++ WillyMVVM_SwiftUI.xctemplate/___VARIABLE_moduleIdentifier___ViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+import SwiftUI
+
+class ___VARIABLE_moduleIdentifier___ViewModel: ObservableObject {
+    private static func add___VARIABLE_moduleIdentifier___Model() -> ___VARIABLE_moduleIdentifier___Model {
+        ___VARIABLE_moduleIdentifier___Model()
+    }
+    
+    @Published private var model: ___VARIABLE_moduleIdentifier___Model = add___VARIABLE_moduleIdentifier___Model()
+}
